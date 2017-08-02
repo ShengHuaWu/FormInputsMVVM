f8941e354e8afca5009bfcb77b1e563db03fa1b6
ShengHua Wu <fantasy0404@gmail.com>
2017-08-02 18:37:10 +0100

    Project initialization

---
+++ FormInputsMVVM/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FormInputsMVVM
+//
+//  Created by ShengHua Wu on 02/08/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: Properties
+    var window: UIWindow?
+
+    // MARK: Application Delegate
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ FormInputsMVVM/Form/FormViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FormViewController.swift
+//  FormInputsMVVM
+//
+//  Created by ShengHua Wu on 02/08/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+final class FormViewController: UIViewController {
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
